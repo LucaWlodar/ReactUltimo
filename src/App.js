a2b83e327aed5ad946b387ea80cb27492fcb7b74
Luca <d47265521@alumnos.itr.edu.ar>
2024-05-08 11:21:55 -0300

    Ej2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from 'react';
 function App() {
+  const [formData, setFormData] = useState({
+    nombre: '',
+    apellido: '',
+    documento: '',
+    fechaNacimiento: ''
+  });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    window.alert('Formulario enviado');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <h2>Formulario de Registro</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="nombre">Nombre:</label>
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            value={formData.nombre}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="apellido">Apellido:</label>
+          <input
+            type="text"
+            id="apellido"
+            name="apellido"
+            value={formData.apellido}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="documento">Número de documento:</label>
+          <input
+            type="number"
+            id="documento"
+            name="documento"
+            value={formData.documento}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="fechaNacimiento">Fecha de nacimiento:</label>
+          <input
+            type="date"
+            id="fechaNacimiento"
+            name="fechaNacimiento"
+            value={formData.fechaNacimiento}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
-
 export default App;
